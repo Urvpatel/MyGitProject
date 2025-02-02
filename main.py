654253a00f2074d73d17b49c246fd99f6982642a
Urvpatel <urvpatelhitack313@gmail.com>
2025-02-02 15:38:01 -0400

    this is main python logic
    this code give the addition of 2 numbers

---
+++ main.py
@@ -0,0 +1,9 @@
+import helper
+
+def main():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("Sum:", helper.add(a, b))
+
+if __name__ == "__main__":
+    main()
